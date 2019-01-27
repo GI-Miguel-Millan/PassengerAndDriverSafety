db31f4c1f5598fa6f27a71a5218b0949445beead
Miguel Millan <miggy221@gmail.com>
2019-01-27 13:02:34 -0500

    test script that records a single 20 second video

---
+++ PiCamEventDetection/src/circularstream.py
@@ -0,0 +1,46 @@
+# Author: Miguel Millan
+# class: CSI4999 
+import io
+import datetime
+import time
+import random
+import picamera
+
+path="../output/"
+codec="h264"
+extension="h264"
+timeformat="%Y%m%d-%H%M%S"
+testing=1 # set 1 = true, 0 = false
+
+def change_detected():
+    # At the moment we'll use random numbers to simulate event detected.
+    # return random.randint(0,10) == 0
+    return 1
+
+camera = picamera.PiCamera()
+camera.resolution = (256, 256)
+stream = picamera.PiCameraCircularIO(camera, seconds=20)
+camera.start_recording(stream, format=codec)
+
+if testing:
+    camera.start_preview()
+
+try:
+    while True:
+        camera.wait_recording(1)
+        print(camera.frame)
+        if change_detected():
+            # generate a timestamp to use for the file name
+            timestamp = datetime.datetime.now()
+            
+            # temporary, keep recording for 10 seconds and only then write the stream to disk
+            camera.wait_recording(10)
+            filename=path+"{}.".format(timestamp.strftime(timeformat))+extension
+           
+            stream.copy_to(filename)
+            break #temporary, for now just want one recording, then stop
+
+finally:
+    if testing:
+        camera.stop_preview()
+    camera.stop_recording()
