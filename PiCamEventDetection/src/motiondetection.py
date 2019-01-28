7ba3333efc521f270178d4d913d5f12d7bf2d55c
Miguel Millan <miggy221@gmail.com>
2019-01-28 16:54:35 -0500

    finish up the motiondection.py modified example script

--- PiCamEventDetection/src/motiondetection.py
+++ PiCamEventDetection/src/motiondetection.py
@@ -37,6 +37,11 @@ def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
 RES=tuple([640,480])
 FPS=10
 CAM_WARMUP=2.5
+DELTA_THRESH=5
+SHOW_VIDEO=1 # TRUE
+MIN_UPLOAD_SECONDS=3.0
+MIN_MOTION_FRAMES=8
+MIN_AREA=5000
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
@@ -62,7 +67,7 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
     
     # resize the frame, convert it to grayscale, and blur it
     frame = resize(frame, width=500)
-    break
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21,21),0) # to remove high frequence noise (focus on "structural" objects)
 
@@ -76,9 +81,53 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
     # accumulate the weighted average between the current frame and 
     # previous frames, then compute the difference between the current 
     # frame and running average
-    cv2.accumulatedWeighted(gray, avg, 0.5)
+    cv2.accumulateWeighted(gray, avg, 0.5)
     frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg)) # delta = |background_model - current_frame|
     print("framedelta: {}".format(frameDelta))
     print("avg: {}".format(avg))
-    break
     
+    # Threshold the delta image, dilate the thresholded image to fill
+    # in the holes, then find contours on thresholded image
+    thresh = cv2.threshold(frameDelta,DELTA_THRESH, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    
+    # loop over the contours
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < MIN_AREA :
+            continue
+
+        # compute the bounding box for the contour, draw it on the frame,
+        # and update the text
+        (x,y,w,h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+        text = "Occupied"
+
+    # draw the text and timestamp on the frame
+    ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+    cv2.putText(frame, "Room Status: {}".format(text), (10,20), 
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+            0.35, (0, 0, 255), 1)
+    # 
+    #
+    # Put code for storing video if enought motion frames
+    # 
+    #
+    
+    # check to see if the frames should be displayed to the screen
+    if SHOW_VIDEO :
+        # display the security feed
+        cv2.imshow("Security Feed", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the 'q' key is pressed, break from the loop
+        if key == ord("q"):
+            break
+
+    #clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+
+
