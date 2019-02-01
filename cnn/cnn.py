5d7aa5dec38907c3e70c21e6bdda3d52850f93a4
colcampbe3 <cccampbell@oakland.edu>
2019-01-31 22:18:38 -0500

    Add CNN feeding into RNN draft #Colin

--- cnn/cnn.py
+++ cnn/cnn.py
@@ -1,52 +1,85 @@
-import numpy as np
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
-from data import getData
+from keras.layers import Dense, Flatten, Dropout, ZeroPadding3D
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential, load_model
+from keras.optimizers import Adam, RMSprop
+from keras.layers.wrappers import TimeDistributed
+from keras.layers.convolutional import (Conv2D, MaxPooling3D, Conv3D,
+    MaxPooling2D)
+from collections import deque
+import sys
 
-tf.logging.set_verbosity(tf.logging.INFO)
+class CNNModel():
+    def __init__(self, nb_classes, model, seq_length,
+                 saved_model=None, features_length=2048):
+        # Set defaults.
+        self.seq_length = seq_length
+        self.load_model = load_model
+        self.saved_model = saved_model
+        self.nb_classes = nb_classes
+        self.feature_queue = deque()
 
-def cnn_model(features, labels, mode): 
-    #input layer
-    input_layer = tf.reshape(features["x"], [-1, 300, 400, 1])
+        # Set the metrics. Only use top k if there's a need.
+        metrics = ['accuracy']
+        if self.nb_classes >= 10:
+            metrics.append('top_k__categor_ical_accuracy')
 
-    #convolutional layer
-    conv1 = tf.layers.conv2d(inputs=input_layer, filters=32, kernel_size=[5, 5], padding='same', activation=tf.nn.relu)
+        # Either use model that has already been made to build on it, or use new one
+        if self.saved_model is not None:
+            print("Loading model %s" % self.saved_model)
+            self.model = load_model(self.saved_model
+        else:
+            self.model = load_model(self.saved_model)
+            self.input_shape = (seq_length, 80, 80, 3)
+            self.model = self.lrcn()
 
-    #pooling layer
-    pool1 = tf.layers.average_pooling2d(inputs=conv1, pool_size=[5, 5], strides=2)
+        # Now compile the network.
+        optimizer = Adam(lr=1e-5, decay=1e-6)
+        self.model.compile(loss='categorical_crossentropy', optimizer=optimizer,
+                           metrics=metrics)
 
-    #dense layer
-    dense = tf.layers.dense(inputs=pool1 units=1024, activation=tf.nn.relu)
-    dropout = tf.layers.dropout(inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+    def lrcn(self):
+        # Based on model from https://github.com/harvitronix/five-video-classification-methods/blob/master/models.py    """
+        # Specifies 
+        model = Sequential()
 
-    #output
-    logits = tf.layers.dense(inputs=dropout, units=4)
-    predictions = {
-        "classes": tf.argmax(input=logits, axis=1), 
-        "probabilities": tf.nn.softmax(logits, name='softmax_tensor')
-    }
- 
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+        # Build a convolutional neural network. Time-distributed over 2D frames.
+        # Two convolutional layers followed by pooling layer.
+        # Keras convolutional layers guide guide: https://keras.io/layers/convolutional/
+        model.add(TimeDistributed(Conv2D(32, (7, 7), strides=(2, 2),
+            activation='relu', padding='same'), input_shape=self.input_shape))
+        model.add(TimeDistributed(Conv2D(32, (3,3),
+            kernel_initializer="he_normal", activation='relu')))
+        model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
 
-    #calculate Loss
-    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+        model.add(TimeDistributed(Conv2D(64, (3,3),
+            padding='same', activation='relu')))
+        model.add(TimeDistributed(Conv2D(64, (3,3),
+            padding='same', activation='relu')))
+        model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
 
-    #Training op for TRAIN mode
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-        train_op = optimizer.minimize(
-            loss=loss,
-            global_step=tf.train.get_global_step())
-        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+        model.add(TimeDistributed(Conv2D(128, (3,3),
+            padding='same', activation='relu')))
+        model.add(TimeDistributed(Conv2D(128, (3,3),
+            padding='same', activation='relu')))
+        model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
 
-    #Evaluation metrics for EVAL mode
-    eval_metric_ops = {
-        "accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])}
-    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+        model.add(TimeDistributed(Conv2D(256, (3,3),
+            padding='same', activation='relu')))
+        model.add(TimeDistributed(Conv2D(256, (3,3),
+            padding='same', activation='relu')))
+        model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
+        
+        model.add(TimeDistributed(Conv2D(512, (3,3),
+            padding='same', activation='relu')))
+        model.add(TimeDistributed(Conv2D(512, (3,3),
+            padding='same', activation='relu')))
+        model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
 
-def main(unused_argv):
-    #Here is where we set our parameters and run the code on the image
+        model.add(TimeDistributed(Flatten()))
 
-if __name__ == "__main__":
-    tf.app.run()
+        model.add(Dropout(0.5))
+        # Feed into a LSTM, which is a recurrent neural network layer
+        model.add(LSTM(256, return_sequences=False, dropout=0.5))
+        model.add(Dense(self.nb_classes, activation='softmax'))
+
+        return model
