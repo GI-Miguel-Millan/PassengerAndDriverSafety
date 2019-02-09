9b77c182ce977e717d0c2e1d22b7001ee99cd3bd
jisrow <DoctorAtom7@gmail.com>
2019-02-08 19:42:30 -0500

    Add support for static files.

--- webapp/pds/settings.py
+++ webapp/pds/settings.py
@@ -120,3 +120,7 @@ LOGOUT_REDIRECT_URL = 'index'
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
