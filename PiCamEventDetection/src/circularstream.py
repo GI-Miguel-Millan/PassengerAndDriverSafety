cda6d7feb892eed5392e06c377b7f356ebfc287a
mjmillan <miggy221@gmail.com>
2019-02-16 22:13:25 -0500

    adjusted parameters for motion detection, and fps during video saving such that the recorded video is no longer sped up.

--- PiCamEventDetection/src/circularstream.py
+++ PiCamEventDetection/src/circularstream.py
@@ -5,6 +5,20 @@ import datetime
 import time
 import random
 import picamera
+import argparse
+import warnings
+import json
+from picamera.array import PiRGBArray
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--conf", required=True, help="path to the JSON configuration file")
+args = vars(ap.parse_args())
+
+# filter warnings, and load the configuration
+warnings.filterwarnings("ignore")
+conf = json.load(open(args["conf"]))
+
 
 path="../output/"
 codec="h264"
@@ -12,22 +26,23 @@ extension="mpeg"
 timeformat="%Y%m%d-%H%M%S"
 testing=1 # set 1 = true, 0 = false
 
+
 def change_detected():
     # At the moment we'll use random numbers to simulate event detected.
     # return random.randint(0,10) == 0
     return 1
 
+
 camera = picamera.PiCamera()
-camera.resolution = (640,480)
+camera.resolution = tuple(conf["RES"])
 stream = picamera.PiCameraCircularIO(camera, seconds=60)
-camera.start_recording(stream, format=codec)
+camera.start_recording(stream, format=conf["CODEC"])
 
 if testing:
     camera.start_preview()
 
 try:
     while True:
-        camera.wait_recording(1)
         print(camera.frame)
         if change_detected():
             # generate a timestamp to use for the file name
@@ -35,7 +50,7 @@ try:
             
             # temporary, keep recording for 10 seconds and only then write the stream to disk
             camera.wait_recording(10)
-            filename=path+"{}.".format(timestamp.strftime(timeformat))+extension
+            filename = conf["OUTPATH"]+"{}.".format(timestamp.strftime(conf["TIMEFORMAT"]))+conf["EXTENSION"]
            
             stream.copy_to(filename)
             break #temporary, for now just want one recording, then stop
