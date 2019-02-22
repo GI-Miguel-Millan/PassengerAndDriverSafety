fbade6eacff0b125ae8244996b43c3ed3f744900
leviorlando <32489551+leviorlando@users.noreply.github.com>
2019-02-22 18:05:28 -0500

    login page, logout in navbar fixed

--- webapp/pds/urls.py
+++ webapp/pds/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-	path('accounts/', include('django.contrib.auth.urls')),
+	path('admin/', admin.site.urls),
+	path('', include('django.contrib.auth.urls')),
 	path('', TemplateView.as_view(template_name='index.html'), name='index'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
+]
