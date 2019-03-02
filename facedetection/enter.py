8ea935c97740ca7e2bfe8d6126a5e8217ed05d6b
mjmillan <miggy221@gmail.com>
2019-03-02 10:22:06 -0500

    implement entering/exiting detection. TODO: take images, crop faces, send (croped image, entering/exiting status) to the cloud.

--- facedetection/enter.py
+++ facedetection/enter.py
@@ -24,6 +24,10 @@ def avg_joy_score(faces):
         return sum(face.joy_score for face in faces) / len(faces)
     return 0.0
 
+def crop_face(camera, bounding_box):
+    # TODO take a picture and crop the face out of it.
+    return None
+
 def main():
     """Face detection camera inference example."""
     parser = argparse.ArgumentParser()
@@ -41,6 +45,8 @@ def main():
                         help='Camera Frames Per Second')
     parser.add_argument('--region', nargs=4, type=int, default=[1040,600, 600, 632],
                         help='Region for entering/exiting face detection: x, y, width, height')
+    parser.add_argument('--enter_side', type=int, default=1,
+                        help='Used to determine which side of the region should be considered "entering": 1 = right, 0 = left')
     args = parser.parse_args()
 
     # Forced sensor mode, 1640x1232, full FoV. See:
@@ -49,8 +55,13 @@ def main():
     with PiCamera(sensor_mode=4, resolution=(args.cam_width, args.cam_height), framerate=args.fps) as camera:
         camera.start_preview()
         
+        # Get the camera width and height
         width = args.cam_width
         height = args.cam_height
+
+        # Get the region center point and two corners
+        r_center = (args.region[0] + args.region[2] / 2, args.region[1] + args.region[3]/2)
+        r_corners = (args.region[0], args.region[0] + args.region[2], args.region[1], args.region[1] + args.region[3])
 
         # Annotator renders in software so use a smaller size and scale results
         # for increased performace.
@@ -58,36 +69,69 @@ def main():
         scale_x = 320 / width
         scale_y = 240 / height
 
+
         # Incoming boxes are of the form (x, y, width, height). Scale and
         # transform to the form (x1, y1, x2, y2).
         def transform(bounding_box):
             x, y, width, height = bounding_box
             return (scale_x * x, scale_y * y, scale_x * (x + width),
                     scale_y * (y + height))
-        while True:
-            with CameraInference(face_detection.model()) as inference:
-                
-                for result in inference.run(args.num_frames):
-                    faces = face_detection.get_faces(result)
-                    annotator.clear()
-                    
-                    annotator.bounding_box(transform(args.region), fill=0)
-                    
-                    for face in faces:
-                        print(args.region)
-                        print(face.bounding_box)
-                        
-                        if(args.region[0] <= face.bounding_box[0] and \
-                           args.region[0] + args.region[2] >= face.bounding_box[0] + face.bounding_box[2] and \
-                           args.region[1] <= face.bounding_box[1] and \
-                           args.region[1] + args.region[3] >= face.bounding_box[1] + face.bounding_box[3]):
-                            annotator.bounding_box(transform(face.bounding_box), fill=0)
-                        
-                    annotator.update()
 
-                    #print('#%05d (%5.2f fps): num_faces=%d, avg_joy_score=%.2f' %
-                     #   (inference.count, inference.rate, len(faces), avg_joy_score(faces)))
+
+        # Both incoming boxes and the user defined region are of the form (x, y, width, height).
+        # Determines whether the face is entering or exiting the region.
+        def get_status(bounding_box):
+            face_center = (bounding_box[0] + bounding_box[2]/2, bounding_box[1] + bounding_box[3]/2)
+            if face_center[0] > r_center[0]:
+                if args.enter_side == 0:
+                    return True
+                else:
+                    return False
+            else:
+                if args.enter_side == 0:
+                    return False
+                else:
+                    return True
+
+
+        previous_faces = []
+        num_previous_faces = 0
+        num_faces = 0
+        faces = []
+        with CameraInference(face_detection.model()) as inference:
+            
+            for result in inference.run(args.num_frames):
+
+                num_previous_faces = num_faces
+                faces = face_detection.get_faces(result)
+
+                annotator.clear()    
+                annotator.bounding_box(transform(args.region), fill=0)
                 
+                num_faces = 0
+                tmp_arr = []
+                for face in faces:
+                    face_center = (face.bounding_box[0] + face.bounding_box[2]/2, face.bounding_box[1] + face.bounding_box[3]/2)
+                    
+                    # check if the center of the face is in our region of interest:
+                    if (face_center[0] >= r_corners[0] and face_center[0] <= r_corners[1] and  
+                        face_center[1] >= r_corners[2] and face_center[1] <= r_corners[3]) :
+                        
+                        num_faces = num_faces + 1
+
+                        tmp_arr.append([crop_face(camera, face.bounding_box), get_status(face.bounding_box)]) # creates a tuple ( image of the face, entering/exiting status)  
+                        annotator.bounding_box(transform(face.bounding_box), fill=0) # draw a box around the face 
+
+
+                annotator.update()
+
+                if num_faces < num_previous_faces:
+                    # loop through previous faces: send face data, image and status
+                    print (" A face left the region: send previous face data")
+                    for face in previous_faces:
+                        print(face)
+
+                previous_faces = tmp_arr
 
         camera.stop_preview()
 
