b0670c75df0941af9decb880edf5ae4c02978f98
colcampbe3 <cccampbell@oakland.edu>
2019-03-10 19:15:42 -0400

    Create create scripts

---
+++ facial-recognition/README.md
@@ -0,0 +1,5 @@
+In order to run these a couple things need to be configured:
+
+1. Add the subscription key to the code in the parts of the header of all these requests
+
+2. make sure to point the url 
---
+++ facial-recognition/createPerson.py
@@ -0,0 +1,21 @@
+import argparse
+import requests
+import json
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('group')
+parser.add_argument('person')
+args = parser.parse_args()
+
+if args.group is '' or args.person is '':
+    print('Missing arguments')
+else:
+    headers = {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': '[key]',
+    }
+    data = {
+        'name': args.person
+    }
+    response = requests.post("https://eastus.api.cognitive.microsoft.com/face/v1.0/persongroups/%s/persons" % args.group, data=json.dumps(data), headers=headers)
