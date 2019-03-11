e630e141ca864f7fb63b71aa2f6eb82b3ab0a7b5
colcampbe3 <cccampbell@oakland.edu>
2019-03-10 23:01:04 -0400

    Add logging for detected face id

--- facial-recognition/detectFace.py
+++ facial-recognition/detectFace.py
@@ -18,5 +18,9 @@ data = {
     'url': args.img
 }
 response = requests.post(url, data=json.dumps(data), headers=headers)
-
+print(response.status_code)
+print(response.text)
+if response.status_code == 200:
+    with open('personIds/temp.json', 'w') as f:
+        json.dump(response.json(), f)
 # TODO: response contains the faceID, faceRectangle, and a whole bunch of other face landmarks.
