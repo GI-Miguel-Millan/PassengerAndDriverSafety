6c0681fdc567faceab8feb519a2bbe3e37f684c4
unknown <DoctorAtom7@gmail.com>
2019-03-12 22:30:22 -0400

    Forgot to delete stuff. :^)

--- webapp/videos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('upload/', views.upload_video, name='upload'),
-    path('', views.video_list, name='list'),
-    path('<int:video_id>/', views.vote, name='vote'),
-]
