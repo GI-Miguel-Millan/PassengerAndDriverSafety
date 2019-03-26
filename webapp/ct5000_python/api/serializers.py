a91603d0135993cd47cf9a3a361eb7cd52799122
Fabian LeFevre <35782836+Faboover@users.noreply.github.com>
2019-03-25 22:45:50 -0400

    Updated Serializers.py
    Update was made to include the id fields of database entries. This needed to be done because specific fields of the models are references to these id fields.
    
    Makes it easier to find and search through the models on the front end if these fields are returned as a part of the json objects.

--- webapp/ct5000_python/api/serializers.py
+++ webapp/ct5000_python/api/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'is_active')
+        fields = ('id','username', 'first_name', 'last_name', 'email', 'is_active')
         read_only_fields = ('last_login','date_joined')
 
 class ParentSerializer(serializers.ModelSerializer):
@@ -21,24 +21,24 @@ class DeviceSerializer(serializers.ModelSerializer):
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ('first_name','last_name','age','grade','school','bus','picture','parent_one','parent_two', 'track')
+        fields = ('id','first_name','last_name','age','grade','school','bus','picture','parent_one','parent_two', 'track')
 		
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ('enter','picture','device','student')
+        fields = ('enter','timestamp','picture','device','student')
 
 class BusSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bus
-        fields = ('name',)
+        fields = ('id','name',)
         
 class DriverSerializer(serializers.ModelSerializer):
     class Meta:
         model = Driver
-        fields = ('bus','first_name','last_name')
+        fields = ('id','bus','first_name','last_name')
 
 class SchoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
-        fields = ('name','address','city','state','zipcode')
+        fields = ('id','name','address','city','state','zipcode')
